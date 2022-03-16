14501408502a6a13ffa9bb0912f3558757ba99a0
vuoso-l <vuosolucas@gmail.com>
2022-03-16 17:37:07 -0300

    Deleted AddressService from the request

--- C15_S-ejercicio_simil_parcial/src/exerc/Main.java
+++ C15_S-ejercicio_simil_parcial/src/exerc/Main.java
@@ -1,2 +1,25 @@
-package exerc;public class Main {
+package exerc;
+
+import exerc.daos.PatientDaoH2;
+import exerc.models.Address;
+import exerc.models.Patient;
+import exerc.servicies.PatientService;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Address dom1 = new Address("bol", 123, "www", "eee");
+        Patient pat1 = new Patient("vuoso", "bbb", 123456789, new Date(2022, 03, 01), dom1);
+
+        PatientService patService1 = new PatientService();
+
+        //Set a sentence to persist (DAO)
+        patService1.setPatientDao(new PatientDaoH2());
+        //patService1.registerPatient(pat1);
+        patService1.findOne(1L);
+        //patService1.deletePatient(7L);
+        //patService1.findAll();
+
+    }
 }
